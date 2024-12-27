4431aa4e3bc48b2cecb7643bb8463e3f6ff2b13e
2814109 <2814109k@gmail.com>
2024-12-27 10:18:06 +0900

    chore

--- main.ts
+++ main.ts
@@ -1,8 +1,60 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Project, SyntaxKind } from "ts-morph";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+import type { VariableDeclaration, ObjectLiteralExpression, SourceFile } from "ts-morph";
+
+// プロジェクトとソースファイルを初期化
+const project = new Project();
+const sourceFiles = project.addSourceFilesAtPaths(
+[  "./datasources/sample.ts" ]
+);
+
+const getArrayLiteralDeclarations = (sourceFile: SourceFile[]): VariableDeclaration[] =>
+  sourceFile.flatMap((e) => e.getVariableDeclarations()
+    .filter((declaration) =>
+      declaration.getInitializer()?.getKind() === SyntaxKind.ArrayLiteralExpression
+    )
+)
+ 
+const collectObjectLiterals = (declarations: VariableDeclaration[]): ObjectLiteralExpression[] =>
+    declarations.flatMap((declaration) => {
+    const initializer = declaration.getInitializer();
+    const arrayLiteral = initializer?.asKind(SyntaxKind.ArrayLiteralExpression);
+    return arrayLiteral
+      ? arrayLiteral.getElements().filter((element) =>
+          element.getKind() === SyntaxKind.ObjectLiteralExpression
+        ) as ObjectLiteralExpression[]
+      : [];
+  });
+
+const removeProperties = (objectLiterals: ObjectLiteralExpression[], properties: string[]) =>
+    objectLiterals.forEach((objectLiteral, idx) => {
+      console.log({rest : `${idx}/${objectLiterals.length}`})
+    properties.forEach((propName) => {
+      const property = objectLiteral.getProperty(propName);
+      if (property) {
+        property.remove();
+      }
+    });
+  });
+
+const main = async () => {
+    console.log("step1")
+    const declarations = getArrayLiteralDeclarations(sourceFiles);
+    console.log("step2")
+    const objectLiterals = collectObjectLiterals(declarations);
+    console.log("step3")
+    console.log({count : objectLiterals.length})
+  removeProperties(objectLiterals, ["id", "degree", "searchKey"]);
+    console.log("step4")
+
+  try {
+    await sourceFiles.forEach(e => e.save());
+    console.log("Updated file saved!");
+  } catch (error) {
+    console.error("Error saving file:", error);
+  }
+};
+
+// 実行
+main();
+
